2a8516c2a563d386f38829782d97f9332f823dce
jinGGo007 <73293452+jinGGo007@users.noreply.github.com>
2022-05-24 00:09:45 +0800

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -86,7 +86,7 @@ echo -e "============================================="
 echo -e " ${green} INSTALLING SSH && OVPN && WS ${NC}"
 echo -e "============================================="
 sleep 2
-wget https://raw.githubusercontent.com/jinGGo007/PRIVATE/main/V1/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/jinGGo007/LIFETIME/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
 sleep 2
 clear
 
